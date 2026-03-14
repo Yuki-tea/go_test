619055f4f037a50e04062a5389d3aacb95877b31
Yuki-tea <yuuki4168@gmail.com>
2026-03-14 22:44:24 +0900

    imple: sub, mul, div operations

--- playground/calculator/main.go
+++ playground/calculator/main.go
@@ -5,5 +5,12 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to the Go Calculator")
 	sum := add(10, 5)
+	sub := sub(10, 4)
+	div := div(10, 2)
+	mul := mul(2, 5)
+	
 	fmt.Println("10 + 5 =", sum)
+	fmt.Println("10 - 4 =", sub)
+	fmt.Println("10 / 2 =", div)
+	fmt.Println("2 * 5 =", mul)
 }
--- playground/calculator/math.go
+++ playground/calculator/math.go
@@ -1,5 +1,17 @@
 package main
 
-func add(a, b int) int{
+func add(a, b int) int {
 	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func mul(a, b int) int {
+	return a * b
+}
+
+func div(a, b int) int {
+	return a / b
 }
