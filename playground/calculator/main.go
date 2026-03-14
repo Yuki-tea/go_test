1e895ba7a543e8294fdcf1a2f60e234e79550558
Yuki-tea <yuuki4168@gmail.com>
2026-03-14 23:20:51 +0900

    impl: validation for div

--- playground/calculator/main.go
+++ playground/calculator/main.go
@@ -5,12 +5,22 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to the Go Calculator")
 	sum := add(10, 5)
-	sub := sub(10, 4)
-	div := div(10, 2)
-	mul := mul(2, 5)
+	difference := sub(10, 4)
+	quotient, err := div(10, 2)
+	product := mul(2, 5)
 	
 	fmt.Println("10 + 5 =", sum)
-	fmt.Println("10 - 4 =", sub)
-	fmt.Println("10 / 2 =", div)
-	fmt.Println("2 * 5 =", mul)
+	fmt.Println("10 - 4 =", difference)
+	fmt.Println("2 * 5 =", product)
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println("10 / 2 =", quotient)
+	}
+	quotient, err = div(10, 0)
+	if err != nil{
+		fmt.Println(err)
+	} else {
+		fmt.Println("10 / 0 =", quotient)
+	}
 }
--- playground/calculator/math.go
+++ playground/calculator/math.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 func add(a, b int) int {
 	return a + b
 }
@@ -12,6 +14,9 @@ func mul(a, b int) int {
 	return a * b
 }
 
-func div(a, b int) int {
-	return a / b
+func div(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by 0")
+	}
+	return a / b, nil
 }
