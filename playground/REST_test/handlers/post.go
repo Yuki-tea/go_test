40eb2cb38a7caca242065522190ea79d53de79be
Yuki-tea <yuuki4168@gmail.com>
2026-03-18 19:26:56 +0900

    refact: separate the request handler

---
+++ playground/REST_test/handlers/post.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	// custom package
+	"rest-api/db"
+)
+
+// the fields must start with a capital letter to make it public
+// the struct tags show how to translate the data into JSON format
+type BlogPost struct {
+	ID	int `json:"id"`
+	Title string `json:"title"`
+	Content string `json:"content"`
+}
+
+func GetPostHandler(w http.ResponseWriter, r *http.Request) {
+	var post BlogPost
+
+	row := db.DB.QueryRow("SELECT id, title, content From blog_posts WHERE id = 1")
+	// need to pass the pointers
+	err := row.Scan(&post.ID, &post.Title, &post.Content)
+
+	if err != nil {
+		http.Error(w, "Failed to fetch post from database", http.StatusInternalServerError)
+		return
+	}
+	// tell the browser we are sending JSON, not a plain text
+	w.Header().Set("Content-Type", "application/json")
+	// encode thte Go struct into JSON and send it
+	json.NewEncoder(w).Encode(post)
+}
--- playground/REST_test/main.go
+++ playground/REST_test/main.go
@@ -4,18 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
 
 	// custom packages
 	"rest-api/db"
+	"rest-api/handlers"
 )
-// the fields must start with a capital letter to make it public
-// the struct tags show how to translate the data into JSON format
-type BlogPost struct {
-	ID	int `json:"id"`
-	Title string `json:"title"`
-	Content string `json:"content"`
-}
 
 func main() {
 	db.Init()
@@ -25,20 +18,7 @@ func main() {
 		fmt.Fprintf(w, "The database connection was successful!")
 	})
 
-	http.HandleFunc("/api/post", func(w http.ResponseWriter, r *http.Request) {
-		var post BlogPost
-		row := db.DB.QueryRow("SELECT id, title, content From blog_posts WHERE id = 1")
-		// need to pass the pointers
-		err := row.Scan(&post.ID, &post.Title, &post.Content)
-		if err != nil {
-			http.Error(w, "Failed to fetch post from database", http.StatusInternalServerError)
-			return
-		}
-		// tell the browser we are sending JSON, not a plain text
-		w.Header().Set("Content-Type", "application/json")
-		// encode thte Go struct into JSON and send it
-		json.NewEncoder(w).Encode(post)
-	})
+	http.HandleFunc("GET /api/post", handlers.GetPostHandler)
 
 	fmt.Println("Web server is starting on port 8080...")
 	// ListenAndServe blocks the program from exiting. We wrap it in log.Fatal 
