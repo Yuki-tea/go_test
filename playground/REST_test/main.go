6893df7c646e02d19be7d57d1228a0cb55ef964a
Yuki-tea <yuuki4168@gmail.com>
2026-03-19 18:48:32 +0900

    feat: CORS setting

--- playground/REST_test/main.go
+++ playground/REST_test/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"github.com/rs/cors"
 
 	// custom packages
 	"rest-api/db"
@@ -33,8 +35,20 @@ func main() {
 	http.HandleFunc("PUT /api/posts/{id}", postHandler.PutPostHandler)
 	http.HandleFunc("PATCH /api/posts/{id}", postHandler.PatchPostHandler)
 
+	allowedOrigin := os.Getenv("FRONTEND_URL")
+	c := cors.New(cors.Options{
+		// explicitly allow the Next.js default port
+		AllowedOrigins: []string{allowedOrigin},
+		AllowedMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
+	
+	// wrap default router in the CORS 
+	protectedHandler := c.Handler(http.DefaultServeMux)
+
 	fmt.Println("Web server is starting on port 8080...")
+
 	// ListenAndServe blocks the program from exiting. We wrap it in log.Fatal 
 	// so if the server crashes, it prints the error and exits gracefully.
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", protectedHandler))
 }
